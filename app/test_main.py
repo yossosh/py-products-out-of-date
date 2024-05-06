41f41b09e528d75bd3fa4d70b3539c7b80bbd1f2
Sofiia <sonagm9@gmail.com>
2024-05-06 15:17:07 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,29 @@
-# write your code here
+import datetime
+from unittest import mock
+
+from app.main import outdated_products
+
+
+@mock.patch("app.main.datetime")
+def test_should_return_out_of_date(mocked_date: mock.MagicMock) -> None:
+    products = [
+        {
+            "name": "salmon",
+            "expiration_date": datetime.date(2022, 2, 10),
+            "price": 600
+        },
+        {
+            "name": "chicken",
+            "expiration_date": datetime.date(2022, 2, 5),
+            "price": 120
+        },
+        {
+            "name": "duck",
+            "expiration_date": datetime.date(2022, 2, 1),
+            "price": 160
+        }
+    ]
+
+    mocked_date.date.today.return_value = datetime.date(2022, 2, 2)
+
+    assert outdated_products(products) == ["duck"]
